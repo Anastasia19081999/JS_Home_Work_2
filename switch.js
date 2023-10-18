7156d16246ef9a7347852aa012e6cedbce93fb33
Anastasia19081999 <anastasia121598@gmail.com>
2023-10-18 20:27:23 +0300

    switch

--- switch.js
+++ switch.js
@@ -2,17 +2,25 @@
 
 
 
-// let sunday = prompt('What day is Sunday now?');
+let sunday = prompt('What day is Sunday now?');
 
-// switch (sunday) {
-//     case Monday:
-//         alert('Today is labor day');
-//         break;
-//     case Tuesday:
-//     case Thursday:
-//         alert('Today' ${sunday} 'you have class at 19:00 p.m');
-//         break;
-//     case Wednesday:
-//         alert('family day');
-//         break;
-// }
+switch (sunday) {
+    case 'Monday':
+        alert('Today is labor day');
+        break;
+    case 'Tuesday':
+        alert(`Today ${sunday} you have class at 19:00 p.m`);
+        break;
+    case 'Wednesday':
+        alert('family day');
+        break;
+    case 'Thursday':
+            alert(`Today ${sunday} you have class at 19:00 p.m`);
+            break;
+    case 'Friday':
+        alert('Food day');
+        break;
+    default:
+        alert('There is no such day');
+        break;
+}
