ec7803e1cde5773c793e7e09905dfc20f15ecdc9
Anastasia19081999 <anastasia121598@gmail.com>
2023-10-18 19:23:12 +0300

    userAge

---
+++ switch.js
@@ -0,0 +1,18 @@
+// створити змінну в якій за допогою Prompt спитати який день неділі зараз. Далі написати swith який для вівторок четверг значень виводить ( Сьогодні ${day} у вас урок о 19) для інших днів свої варіанти,  адля значень що не відповідають умові виведіть default
+
+
+
+// let sunday = prompt('What day is Sunday now?');
+
+// switch (sunday) {
+//     case Monday:
+//         alert('Today is labor day');
+//         break;
+//     case Tuesday:
+//     case Thursday:
+//         alert('Today' ${sunday} 'you have class at 19:00 p.m');
+//         break;
+//     case Wednesday:
+//         alert('family day');
+//         break;
+// }
---
+++ userAge.js
@@ -0,0 +1,18 @@
+//  Спитати в користувача вік, далі використовуючи тернарний оператор ( це скорочений запис іf ) створити змінну яка отримує true якщо вік більше 18  i false якщо менше
+
+
+let userAge = prompt('How old are you ?');
+
+
+
+result = userAge >= '18' ? "true" : 'false';
+console.log(result);
+
+
+// let userAge = prompt('How old are you ?');
+
+// if(userAge >= '18'){
+//     alert('true');
+// } else {
+//     alert('false');
+// }
